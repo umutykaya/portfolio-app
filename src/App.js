3dfb380b338cb7c3bf80911671de2cfe0f27f780
yalcinkaya, Umut (Commencis) <umut.yalcinkaya@commencis.com>
2020-09-28 03:38:26 +0300

    pipeline

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { API } from 'aws-amplify';
+
 function App() {
+  const [greetings, setGreeting] = useState(null)
+  async function fetchGreeting() {
+    const apiData = await API.get('greetingsapi','/greetings')
+    setGreeting(apiData.message)
+  }
+  useEffect(() => {
+    fetchGreeting()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{greetings}</h1>
       </header>
     </div>
   );
